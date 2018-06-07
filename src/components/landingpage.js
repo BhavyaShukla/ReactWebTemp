e849cbb5af2336f3ccbb4752fe0fdfbc41bdee84
Bhavya Shukla <bhavyashukla.dev@gmail.com>
2018-06-07 21:54:45 +0530

    First commit

---
+++ src/components/landingpage.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {Grid,Cell} from 'react-mdl'
+class Landing extends Component{
+  render(){
+    return(
+      <div style={{width:'100%',margin:'auto'}}>
+        <Grid className="landing-grid">
+            <Cell col='12'>
+                <img
+                src="https://i1.wp.com/www.neuroequilibrium.in/wp-content/uploads/2015/12/Male-avatar.png?fit=512%2C512&ssl=1"
+                alt="avatar"
+                className="avatar-img"
+                    />
+
+            </Cell>
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+                <hr/>
+
+              <p>HTML/CSS | Bootstrap | CSSGRID | CSS3 | JavaScript | React | Polymer | Cordova </p>
+
+              <div className="social-links">
+              {/*LinkedIN*/}
+                <a href="http://www.google.com" target="_blank">
+                  <i className="fa fa-linkedin-square"rel="noopener noreferrer" aria-hidden="true"/>
+                </a>
+
+                {/*Facebook*/}
+                <a href="http://www.google.com" target="_blank">
+                  <i className="fa fa-facebook-square"rel="noopener noreferrer" aria-hidden="true"/>
+                </a>
+                {/*Twitter*/}
+                <a href="http://www.google.com" target="_blank">
+                  <i className="fa fa-twitter-square"rel="noopener noreferrer" aria-hidden="true"/>
+                </a>
+                {/*Github*/}
+                <a href="http://www.google.com" target="_blank">
+                  <i className="fa fa-github-square"rel="noopener noreferrer" aria-hidden="true"/>
+                </a>
+              </div>
+
+
+
+            </div>
+        </Grid>
+      </div>
+    )
+  }
+}
+export default Landing;
---
+++ src/components/resume.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import {Grid,Cell} from 'react-mdl';
+import Education from './education';
+import Experience from './experience';
+import Skill from './skill';
+class Resume extends Component{
+  render(){
+    return(
+    <div>
+      <Grid>
+       <Cell className="resume-left-col"col={4}>
+          <img
+            src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+            alt="avatar"
+            style={{height: '200px'}}
+          />
+          <h2>Bhavya Shukla</h2>
+          <h4>Developer|Photographer</h4>
+          <hr/>
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</p>
+          <hr/>
+          <h5>Address</h5>
+            <p>1 Hacker Way Menlo Park, 94025</p>
+            <h5>Phone</h5>
+            <p>(123) 456-7890</p>
+            <h5>Email</h5>
+            <p>someone@example.com</p>
+            <h5>Web</h5>
+            <p>mywebsite.com</p>
+            <hr/>
+
+       </Cell>
+       <Cell className="resume-right-col" col={8}>
+
+        <h2> Education</h2>
+
+                  <Education
+                  startYear={2014}
+                  endYear={2018}
+                  schoolName="Mumbai University"
+                  schoolDescription="The University of Mumbai, informally known as Mumbai University (MU), is one of the earliest state universities in India and the oldest in Maharashtra."
+                  />
+
+
+                  <Education
+                  startYear={2014}
+                  endYear={2018}
+                  schoolName="Mumbai University"
+                  schoolDescription="The University of Mumbai, informally known as Mumbai University (MU), is one of the earliest state universities in India and the oldest in Maharashtra."
+                  />
+                  <hr/>
+                  <h2>Experience</h2>
+                  <Experience
+                  jobstartYear={2015}
+                  jobendYear={2017}
+                  jobName="SLLD Automation"
+                  jobDescription="SLLD provides turnkey automation solutions for process and machine tool industries. With more than 15 years of experience we are the best in solution"
+                  />
+                  <hr/>
+                  <h2>Skills</h2>
+                        <Skill
+                        skill="javascript"
+                        progress={100}
+                          />
+
+                          <Skill
+                          skill="HTML/CSS"
+                          progress={78}
+                            />
+                            <Skill
+                            skill="Bootstrap"
+                            progress={66}
+                              />
+                              <Skill
+                              skill="CSS-GRID"
+                              progress={92}
+                                />
+                                <Skill
+                                skill="VueJS"
+                                progress={30}
+                                  />
+
+
+       </Cell>
+      </Grid>
+    </div>
+    )
+  }
+}
+export default Resume;
